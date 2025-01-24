c757fc666ba55694550f8d263c34e5a2af05f604
ddah0329 <ddah0329m@gmail.com>
2025-01-24 11:51:55 +0900

    Update _02_task.java

--- JAVA_Basic/day02/src/c_variable/_02_task.java
+++ JAVA_Basic/day02/src/c_variable/_02_task.java
@@ -4,8 +4,8 @@ public class _02_task {
 	public static void main(String[] args) {
 		int num = 1;
 		long numL = 0L;
-		float decimalF = 1.0F;
 		double decimal = 0.03;
+		float decimalF = 1.0F;
 		boolean TrueorFalse = true;
 		char spelling = 'M';
 		String name = "ddah";
